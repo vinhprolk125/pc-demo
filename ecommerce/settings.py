becf07d77347d03b88ea20171ec798bcb92c7a7a
vinhprolk125 <85087098+vinhprolk125@users.noreply.github.com>
2021-05-31 09:09:52 +0700

    aaa

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -123,7 +123,7 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
