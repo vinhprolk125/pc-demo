de57634ebc922f27d09a5436de06afc7db0aa1e1
vinhprolk125 <85087098+vinhprolk125@users.noreply.github.com>
2021-05-31 09:11:31 +0700

    a

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,9 +124,9 @@ USE_THOUSAND_SEPARATOR = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static')
+#]
 
 MEDIA_URL = '/images/'
 
